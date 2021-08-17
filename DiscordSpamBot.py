bcbdef8ce9f176c62485213db11fde30eeebba8e
Nathan, H <88916522+nHunter0@users.noreply.github.com>
2021-08-17 20:11:56 +1000

    Update DiscordSpamBot.py

--- DiscordSpamBot.py
+++ DiscordSpamBot.py
@@ -1,22 +1,12 @@
 import discord
-import re
-import random
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as: ', self.user)
-
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
-
-        RNG = random.randint(1, 4) #1/4 times message will send when send
-        
-        if  RNG == 1: 
-
-            rere = re.search('(?i)keyWord' , message.content) #regex search for 'keyWord' in sentances 
-            if rere:
-                await message.channel.send('message') #If 'word' is found "message' will be sent by bot
-            
-client = MyClient()
+client = discord.Client()
+keywords = ["!releaseSpamBot"] #discord command
+@client.event
+async def on_message(message):
+    for i in range(len(keywords)):
+        if keywords[i] in message.content:
+            for j in range(10): #amount of time message will be spamed by bot 
+                await message.channel.send("spamMessage") #message that will be spamed by bot
+                
 client.run('DISCORD_TOKEN')
