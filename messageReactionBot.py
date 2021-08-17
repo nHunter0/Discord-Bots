bdaf631f40617bf4b269825734cdd09ea9ae4168
Nathan, H <88916522+nHunter0@users.noreply.github.com>
2021-08-17 20:34:29 +1000

    Create messageReactionBot.py
    This bot simply regex searches for 'keyWord' and if found sends a message, can be modified to send media files, links, @commands, .gif, ect...

---
+++ messageReactionBot.py
@@ -0,0 +1,22 @@
+import discord
+import re
+import random
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged as: ', self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        RNG = random.randint(1, 4) #1/4 times message will send when send
+        
+        if  RNG == 1: 
+
+            rere = re.search('(?i)keyWord' , message.content) #regex search for 'keyWord'
+            if rere:
+                await message.channel.send('message') #If 'keyWord' is found "message' will be sent by bot
+            
+client = MyClient()
+client.run('DISCORD_TOKEN')
